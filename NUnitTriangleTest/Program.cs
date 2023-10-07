a7332c1b7458dee2e89f91a3096fb315e896f311
HP <griya242k@gmail.com>
2023-10-07 10:16:44 -0400

    Add project files.

---
+++ NUnitTriangleTest/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnitTraingle;
+using NUnit.Framework;
+
+namespace NUnitTriangleTest
+{
+    //Author: Riya Gupta
+    public class NUnitTriangleTest
+    {
+        [TestFixture]
+        public class NUnitTriangleTest1
+        {
+            [Test]
+
+            public void RiyaGupta_ValidTriangle_Input60and60and60_OutputValidTriangle()
+            {
+                //Arrange
+                int firstAngle = 60;
+                int secondAngle = 60;
+                int thirdAngle = 60;
+
+                string expected = "The triangle is valid.";
+
+                //Act
+                String actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+                Console.WriteLine(actual);
+                //Assert
+                Assert.AreEqual(expected, actual);
+            }
+
+            [Test]
+            public void RiyaGupta_InvalidTriangle_InputNegative60and100and140_OutputValidTriangle()
+            {
+                //Arrange
+                int firstAngle = -60;
+                int secondAngle = 100;
+                int thirdAngle = 140;
+
+                string expected = "The triangle is valid.";
+
+                //Act
+                String actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+                Console.WriteLine(actual);
+                //Assert
+                Assert.AreEqual(expected, actual);
+            }
+
+            [Test]
+            public void RiyaGupta_InvalidTriangle_Input0and140and40_OutputValidTriangle()
+            {
+                //Arrange
+                int firstAngle = 0;
+                int secondAngle = 140;
+                int thirdAngle = 40;
+
+                string expected = "The triangle is valid.";
+
+                //Act
+                String actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+                Console.WriteLine(actual);
+                //Assert
+                Assert.AreEqual(expected, actual);
+            }
+
+            public static void Main()
+            {
+
+            }
+
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTraingle
+{
+    public static class Triangle
+    {
+        public static void Main()
+        {
+
+        }
+        public static string ValidTriangle(int firstAngle, int secondAngle, int thirdAngle)
+        {
+            string result;
+            //  The * in first and second angle. Ideally, it should be an addition.
+            if ((firstAngle * secondAngle + thirdAngle) == 180)
+            {
+                result = "The triangle is valid.";
+            }
+            else
+            {
+                result = "The triangle is NOT valid.";
+            }
+            return result;
+        }
+    }
+}
